043050ec507d38d461360e930e7a244c71c1a4e2
Evan Adi <evan.adiprasetyo@gmail.com>
2020-03-11 08:45:15 +0700

    Add feature to read config file

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/pflag v1.0.5
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 
 	botapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	homedir "github.com/mitchellh/go-homedir"
 	flag "github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
 )
+
+type Conf struct {
+	Config struct {
+		APIToken string `yaml:"api-token"`
+	} `yaml:"config"`
+}
+
+var filePath string
 
 func check(e error) {
 	if e != nil {
@@ -18,17 +29,37 @@ func main() {
 	// Change your target account aka Telegram Chat ID
 	var chatID int64 = 154321022
 
-	bot, err := botapi.NewBotAPI("YourAPIToken")
+	var home, err = homedir.Dir()
 	check(err)
 
+	// Sets default path for go-telegram config file
+	var defaultPath = home + "/.go-telegram/config"
+
+	var config = flag.String("config", defaultPath, "Specify the path to config file")
 	var message = flag.String("send-message", "check", "Send message to specified Chat ID")
 	var id = flag.Int64("chat-id", chatID, "Specify the Telegram chat ID to send the message to")
 	flag.Parse()
 
+	var conf = getConf(*config)
+	bot, err := botapi.NewBotAPI(conf.Config.APIToken)
+	check(err)
 	log.Printf("Authorized as %s", bot.Self.UserName)
 
 	msg := botapi.NewMessage(*id, *message)
 
 	bot.Send(msg)
+
+}
+
+func getConf(configPath string) *Conf {
+
+	c := &Conf{}
+	yamlFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+
+	return c
 
 }
